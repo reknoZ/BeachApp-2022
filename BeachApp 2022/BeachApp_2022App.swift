0a2ef3cfc735e9bd595130b28b5d2b50add17005
reknoZ <david@zicount.com>
2023-01-31 11:48:17 +0100

    Initial Commit

---
+++ BeachApp 2022/BeachApp_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  BeachApp_2022App.swift
+//  BeachApp 2022
+//
+//  Created by David on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeachApp_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
